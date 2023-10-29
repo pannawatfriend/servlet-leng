d950732eac6b18b7ce1182c3275926cfabee6c6c
LenGKunG123 <111360772+LenGKunG123@users.noreply.github.com>
2023-10-29 15:12:13 +0700

    Add files via upload

---
+++ classicmodelfri/src/main/java/sit/int202/classicmodelfri/TestEntityRelationship.java
@@ -0,0 +1,39 @@
+package sit.int202.classicmodelfri;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import sit.int202.classicmodelfri.entities.Employee;
+import sit.int202.classicmodelfri.entities.Office;
+
+import java.util.Scanner;
+
+    public class TestEntityRelationship {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Find office by office code (enter 0 to exit): ");
+            String officeCode = sc.next();
+            if (officeCode.equalsIgnoreCase("0")) {
+                break;
+            }
+            Office office = em.find(Office.class, officeCode);
+            if (office == null) {
+                System.out.println("Office number " + officeCode + " does not exist !!");
+            } else {
+                displayOfficeEmployee(office);
+            }
+        }
+    }
+
+    private static void displayOfficeEmployee(Office office) {
+        System.out.printf("%s %s %s %s\n", office.getOfficeCode(), office.getAddressLine1(), office.getCity(), office.getCountry());
+        System.out.println("----------------------------------");
+        for (Employee e : office.getEmployeeList()) {
+            System.out.printf("%4d %-12s %-12s %s\n", e.getEmployeeNumber(), e.getFirstName(), e.getLastName(), e.getJobTitle());
+        }
+        System.out.println();
+    }
+}
---
+++ classicmodelfri/src/main/java/sit/int202/classicmodelfri/entities/Office.java
@@ -0,0 +1,34 @@
+package sit.int202.classicmodelfri.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = {"territory", "addressLine2", "postalCode"})
+@Entity
+@Table(name = "offices")
+@NamedQueries({
+        @NamedQuery(name = "OFFICE.FIND_ALL", query = "select o from Office o "),
+        @NamedQuery(name = "OFFICE.FIND_BY_COUNTRY",
+                query = "select o from Office o where o.country like :countryParam")
+})
+
+public class Office {
+    @Id
+    private String officeCode;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String phone;
+    private String territory;
+    @OneToMany(mappedBy = "officeCode")
+    private List<Employee> employeeList;
+}
